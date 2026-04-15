94f99902ee303a9046a5c36eb8da8b8fe76f59b7
VM <vmtiendaon@gmail.com>
2026-04-15 19:04:28 -0300

    gtm: actualizar ID a GTM-MZSZXFDJ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}
+})(window,document,'script','dataLayer','GTM-MZSZXFDJ');`}
         </Script>
         {/* End Google Tag Manager */}
 
@@ -238,7 +238,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZSZXFDJ"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
